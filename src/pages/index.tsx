7a33e3101a73d35cb138ad59a114dc0b71431359
Pedro <pedrokaz.henrique@gmail.com>
2021-12-29 20:04:56 -0300

    added useForm integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
 import type { NextPage } from "next";
-import {
-  Button,
-  Flex,
-  Input,
-  Stack,
-  FormLabel,
-  FormControl,
-} from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Input } from "../components/Form/Input";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type SignInFormValues = {
+  email: string;
+  password: string;
+};
+const signInFormSchema = yup.object({
+  email: yup.string().required("E-mail obrigatório").email(),
+  password: yup.string().required("Senha obrigatória"),
+});
 
 const SignIn: NextPage = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormValues> = async (
+    values,
+    event
+  ) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -19,13 +37,32 @@ const SignIn: NextPage = () => {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
+          <Input
+            {...register("email")}
+            name="email"
+            type="email"
+            label="E-mail"
+            error={errors.email}
+          />
 
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            {...register("password")}
+            name="password"
+            type="password"
+            label="Senha"
+            error={errors.password}
+          />
 
-          <Button type="submit" size="lg" colorScheme="pink" mt="6">
+          <Button
+            type="submit"
+            isLoading={formState.isSubmitting}
+            size="lg"
+            colorScheme="pink"
+            mt="6"
+          >
             Entrar
           </Button>
         </Stack>
