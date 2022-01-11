f95af44bd18a4417f063087b8ce75d4d6723ef72
Pedro <pedrokaz.henrique@gmail.com>
2022-01-10 22:34:15 -0300

    useUser hook

--- src/services/hook/useUsers.ts
+++ src/services/hook/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query";
 import { api } from "../api";
 interface UsersType {
   name: string;
@@ -37,13 +37,18 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
 }
 
 export function useUsers(page: number) {
-  return useQuery(
-    "users",
-
-    () => getUsers(page),
-
-    {
-      staleTime: 1000 * 5, //five seconds
-    }
-  );
+  return useQuery(["users", { page }], () => getUsers(page), {
+    staleTime: 1000 * 60 * 10, //five seconds
+  });
 }
+
+// export function useUsers(
+//   page: number,
+//   options?: UseQueryOptions<GetUsersResponse, UsersType, any, any>
+// ) {
+//   return useQuery(["users", page], () => getUsers(page), {
+//     staleTime: 1000 * 60 * 1,
+//     ...options,
+//   });
+//   //as UseQueryResult<GetUsersResponse, unknown>
+// }
